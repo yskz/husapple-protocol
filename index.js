60950dfcf6d937da4aa3849d6c7a19310977e43d
yskz <yskz@metamagic.co.jp>
2020-09-10 22:10:19 +0900

    feat: ゲーム情報関連のクラスをexportするようにしました。

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const message = require('./src/message');
+const gameInfo = require('./src/game_info');
 
 module.exports = {
     ErrorId: message.ErrorId,
     playerNameMaxLength: message.playerNameMaxLength,
     Message: message,
+    GameInfo: gameInfo,
 };
